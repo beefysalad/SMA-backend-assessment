392cbb5e474fb21191aa6a74c3715954e96c0ee8
beefysalad <jpatrickzephyr@gmail.com>
2026-03-14 04:35:19 +0800

    fix: prisma.js not having access to env variable

--- src/utils/prisma.js
+++ src/utils/prisma.js
@@ -1,5 +1,8 @@
+const dotenv = require("dotenv");
 const { PrismaClient } = require("@prisma/client");
 const { PrismaPg } = require("@prisma/adapter-pg");
+
+dotenv.config();
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
